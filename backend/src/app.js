c15dea59c1c8ab90cdf9949d3fd23978ccf4da27
Pratyush Chaudhary <pratyushchaudhary4002@gmail.com>
2025-07-10 23:49:11 +0530

    new commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,11 @@
 import express from "express"
 import {createServer} from "node:http"
 import {Server} from "socket.io"
+
+
 import mongoose from "mongoose"
 import cors from "cors"
+
 import { connectToSocket } from "./controllers/socketManager.js"
 const app=express();
 const server=createServer(app);
